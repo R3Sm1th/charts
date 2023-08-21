1c81098d9e39d2fb452fe32a942f593709a6ed09
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-08-21 11:17:10 +0100

    chartzoom causing issues

--- app/javascript/controllers/line_chart_controller.js
+++ app/javascript/controllers/line_chart_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import zoomPlugin from "chartjs-plugin-zoom"
+import ChartZoom from "chartjs-plugin-zoom"
 import Chart from "chart.js/auto"
 import "chartjs-adapter-date-fns"
 
@@ -9,38 +9,67 @@ export default class extends Controller {
   static targets = ["canvas"]
 
   connect() {
-    Chart.register(zoomPlugin)
-    this.date = new Date().getDate()
+    console.log("connected");
+    Chart.register(ChartZoom)
+    this.date = new Date().getTime()
     this.fetchData(this.date - 7 * 24 * 60 * 60 * 1000, this.date)
-      .then(() => this.intializeChart())
+      .then(() => {
+        this.initializeChart()
+      })
   }
-  intializeChart(){
-    this.chart= new Chart(this.canvasTarget.getContext("2d"), {
+
+  initializeChart() {
+    this.chart = new Chart(this.canvasTarget.getContext("2d"), {
       type: "line",
       data: this.chartData,
       options: {
+
         scales: {
-          x: { type: "time", id: "x-axis"}
+          x: {
+            type: 'time',
+            id: 'x-axis'
+          }
         },
         plugins: {
+          legend: { display: false },
           zoom: {
             pan: {
               enabled: true,
               mode: "x",
+              onPanComplete: function({}) {
+                this.handleChartZoomOrPan()
+              }.bind(this)
             },
-            wheel: {
-              enabled: true,
-            },
-            mode: "x",
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              mode: "x",
+              onZoomComplete: function ({ }) {
+                this.handleChartZoomOrPan()
+              }.bind(this)
+            }
           }
         }
       }
     })
   }
-  fetchData(start_data, end_data){
+
+  handleChartZoomOrPan() {
+    clearTimeout(this.timer)
+    this.timer = setTimeout(() => {
+      this.fetchData()
+        .then(() => {
+          this.chart.data = this.chartData
+          this.chart.update()
+        })
+    }, 500)
+  }
+
+  fetchData(start_time, end_time) {
     let min = start_time || this.chart.options.scales.x.min
     let max = end_time || this.chart.options.scales.x.max
-    let params = new URLSearchParams({start_time: min, end_time: max})
+    let params = new URLSearchParams({ start_time: min, end_time: max })
     let url = `${this.urlValue}?${params.toString()}`
 
     return fetch(url, {
@@ -48,11 +77,11 @@ export default class extends Controller {
       headers: { "Content-Type": "application/json" }
     })
     .then(response => response.json())
-    .then(json => {
+    .then((json) => {
       this.chartData = {
         datasets: [{ data: json }]
       }
     })
-
   }
+
 }
